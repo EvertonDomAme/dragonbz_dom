a9ba4c363a115c65e720a229f2f4e69ce813f401
EvertonDomAme <oliveirameloeverton@gmail.com>
2021-08-16 23:12:52 -0300

    commit do projeto aula 1 - dragon ball z  - CRUD

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import './Main.scss';
+import Navbar from '../Navbar/Navbar';
+
+export default function Main() {
+    return (
+        <>
+        <Navbar/>
+            <h1>YO soy main</h1>
+        </>
+    )
+}
